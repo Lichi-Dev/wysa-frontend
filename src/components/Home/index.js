366a506a2d4e1eceb0172c348d6756ebfa33fb7c
Bonny Boben <bonnybobencr7@gmail.com>
2023-10-29 00:41:44 +0530

    fixed deployment issue due to old package

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,34 @@
 import React from "react";
 import logo from "../../assets/icon.webp";
 import "./index.css";
-import ReactTyped from "react-typed";
 import Button from "../Button";
+import Typed from "typed.js";
 
 const Home = () => {
+  const el = React.useRef(null);
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Hey! I'm <span style='color:#009E99'>wysa</span>"],
+      typeSpeed: 100,
+      showCursor: false,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
   return (
     <div className="home-container">
       <div className="home-sub-container">
         <img className="logo-image" src={logo} />
-        <ReactTyped
+        {/* <Typed
           className="type-text transition"
           strings={["Hey! I'm <span style='color:#009E99'>wysa</span>"]}
           typeSpeed={100}
           startDelay={1000}
-        />
-
+        /> */}
+        <h1 className="type-text transition" ref={el}></h1>
         <p className="para-text transition">
           I'm here to help you sleep better
         </p>
